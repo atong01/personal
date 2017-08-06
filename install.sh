93b0fc301b522caf270f9bed47d7c6e79238136b
Alex Tong <alexanderytong@gmail.com>
2017-08-06 12:02:11 -0700

    Add bash aliases restructure install script

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+git submodule init
+git submodule update
+
+echo "Back up your dotfiles if you want to keep them."
+echo "This script will overwrite them"
+echo ""
+while true; do
+  read -p "Do you want to continue [yn]? " yn
+  case $yn in 
+    [Yy]* )
+      ln -sFf $PWD/vim $HOME/.vim
+      ln -sFf $PWD/dotfiles/vimrc $HOME/.vimrc
+      ln -sFf $PWD/dotfiles/gitconfig $HOME/.gitconfig
+      ln -sFf $PWD/dotfiles/bash_alias $HOME/.bash_alias
+      LINE='. .bash_alias'
+      FILE=$HOME/.bashrc
+      grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+      break;;
+    [Nn]* )
+      exit;;
+    * ) echo "Please answer yes or no.";
+      ;;
+  esac
+done
+echo "Is this a mac? Should I source .bashrc in .bash_profile?"
+echo
+while true; do
+  read -p "Do you want to continue [yn]? " yn
+  case $yn in 
+    [Yy]* )
+      LINE='. .bashrc'
+      FILE=$HOME/.bash_profile
+      grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+      break;;
+    [Nn]* )
+      exit;;
+    * ) echo "Please answer yes or no.";
+      ;;
+  esac
+done
--- vim/install.sh
+++
@@ -1,4 +0,0 @@
-git submodule init
-git submodule update
-ln -sFf $PWD $HOME/.vim
-ln -sFf $PWD/vimrc $HOME/.vimrc
