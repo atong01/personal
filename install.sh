f3b5055ca24728c9513d19ee23506cffa6bbe9b0
Alex Tong <alexandertongdev@gmail.com>
2021-12-15 10:53:06 -0500

    Simplify install.sh script, move from bash_alias to more general alias script, skip mac install

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 git submodule update --init --recursive
 
+
 echo "Back up your dotfiles if you want to keep them."
 echo "This script will overwrite them"
 echo ""
@@ -15,33 +16,10 @@ while true; do
         mv $HOME/.$DOT_FILE $PWD/backup
         ln -sFf $PWD/dotfiles/$DOT_FILE $HOME/.$DOT_FILE
       done
-      LINE='. ~/.bash_alias'
-      FILE=$HOME/.bashrc
-      grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
-      ln -sFf $PWD/bin $HOME/bin
-      break;;
-    [Nn]* )
-      exit;;
-    * ) echo "Please answer yes or no.";
-      ;;
-  esac
-done
-echo "Is this a mac? Should I source .bashrc in .bash_profile?"
-echo
-while true; do
-  read -p "Do you want to continue [yn]? " yn
-  case $yn in 
-    [Yy]* )
-      mv $HOME/.macos $HOME/.mac_alias $PWD/backup
-      ln -sFf $PWD/macfiles/macos $HOME/.macos
-      ln -sFf $PWD/macfiles/mac_alias $HOME/.mac_alias
-      LINE='. ~/.bashrc'
-      FILE=$HOME/.bash_profile
-      grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
-      LINE='. ~/.mac_alias'
-      FILE=$HOME/.bash_profile
-      grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
-      echo 'export PATH="/usr/local/sbin:$PATH"' >> ~/.bash_profile
+      #LINE='. ~/.alias'
+      #FILE=$HOME/.bashrc
+      #grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+      ln -sFf $PWD/bin $HOME/.bin
       break;;
     [Nn]* )
       exit;;
@@ -50,7 +28,31 @@ while true; do
   esac
 done
 
-pip install --user -r requirements.txt
+#echo "Is this a mac? Should I source .bashrc in .bash_profile?"
+#echo
+#while true; do
+#  read -p "Do you want to continue [yn]? " yn
+#  case $yn in 
+#    [Yy]* )
+#      mv $HOME/.macos $HOME/.mac_alias $PWD/backup
+#      ln -sFf $PWD/macfiles/macos $HOME/.macos
+#      ln -sFf $PWD/macfiles/mac_alias $HOME/.mac_alias
+#      LINE='. ~/.bashrc'
+#      FILE=$HOME/.bash_profile
+#      grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+#      LINE='. ~/.mac_alias'
+#      FILE=$HOME/.bash_profile
+#      grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+#      echo 'export PATH="/usr/local/sbin:$PATH"' >> ~/.bash_profile
+#      break;;
+#    [Nn]* )
+#      exit;;
+#    * ) echo "Please answer yes or no.";
+#      ;;
+#  esac
+#done
+
+# pip install --user -r requirements.txt
 
 # OS Specific Install
 
